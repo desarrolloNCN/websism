d74f3c3ec1856df51d48e972eb5e51fdf346261d
Pe-Hs <pedrocsurp22@gmail.com>
2024-02-06 17:37:27 -0500

    descargar data del grafico

--- src/app/modules/uext/pages/lector-demo/lector-demo.component.ts
+++ src/app/modules/uext/pages/lector-demo/lector-demo.component.ts
@@ -636,6 +636,9 @@ export class LectorDemoComponent implements OnInit {
 
   onCloseTab(index: number) {
     this.tabs.splice(index, 1);
+    if (this.tabs.length == 0) {
+      this.hideStaPanel = true
+    }
   }
 
   dateConverter(date: string) {
@@ -715,7 +718,31 @@ export class LectorDemoComponent implements OnInit {
           },
           dataView: { readOnly: true },
           restore: {},
-          saveAsImage: {}
+          saveAsImage: {},
+          myDownloadButton: {
+            show: true,
+            title: 'Descargar Datos',
+            icon: "path://M18.344,16.174l-7.98-12.856c-0.172-0.288-0.586-0.288-0.758,0L1.627,16.217c0.339-0.543-0.603,0.668,0.384,0.682h15.991C18.893,16.891,18.167,15.961,18.344,16.174 M2.789,16.008l7.196-11.6l7.224,11.6H2.789z M10.455,7.552v3.561c0,0.244-0.199,0.445-0.443,0.445s-0.443-0.201-0.443-0.445V7.552c0-0.245,0.199-0.445,0.443-0.445S10.455,7.307,10.455,7.552M10.012,12.439c-0.733,0-1.33,0.6-1.33,1.336s0.597,1.336,1.33,1.336c0.734,0,1.33-0.6,1.33-1.336S10.746,12.439,10.012,12.439M10.012,14.221c-0.244,0-0.443-0.199-0.443-0.445c0-0.244,0.199-0.445,0.443-0.445s0.443,0.201,0.443,0.445C10.455,14.021,10.256,14.221,10.012,14.221",
+            onclick: function () {
+
+              const dataX = value[0].tiempo_a; 
+              const dataY = value[0].traces_a; 
+
+              let dataText = '';
+
+              for (let i = 0; i < dataX.length; i++) {
+                dataText += dataX[i] + '     ' + dataY[i] + '\n'; 
+              }
+
+              const downloadLink = document.createElement('a');
+              downloadLink.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(dataText);
+              downloadLink.download = `DATA_ACELERACION_${e.network}.${e.station}.${e.location}.${e.channel}.txt`;
+
+              document.body.appendChild(downloadLink);
+              downloadLink.click();
+              document.body.removeChild(downloadLink);
+            }
+          }
         }
       },
       grid: {
@@ -812,7 +839,31 @@ export class LectorDemoComponent implements OnInit {
           },
           dataView: { readOnly: true },
           restore: {},
-          saveAsImage: {}
+          saveAsImage: {},
+          myDownloadButton: {
+            show: true,
+            title: 'Descargar Datos',
+            icon: "path://M18.344,16.174l-7.98-12.856c-0.172-0.288-0.586-0.288-0.758,0L1.627,16.217c0.339-0.543-0.603,0.668,0.384,0.682h15.991C18.893,16.891,18.167,15.961,18.344,16.174 M2.789,16.008l7.196-11.6l7.224,11.6H2.789z M10.455,7.552v3.561c0,0.244-0.199,0.445-0.443,0.445s-0.443-0.201-0.443-0.445V7.552c0-0.245,0.199-0.445,0.443-0.445S10.455,7.307,10.455,7.552M10.012,12.439c-0.733,0-1.33,0.6-1.33,1.336s0.597,1.336,1.33,1.336c0.734,0,1.33-0.6,1.33-1.336S10.746,12.439,10.012,12.439M10.012,14.221c-0.244,0-0.443-0.199-0.443-0.445c0-0.244,0.199-0.445,0.443-0.445s0.443,0.201,0.443,0.445C10.455,14.021,10.256,14.221,10.012,14.221",
+            onclick: function () {
+
+              const dataX = value[0].tiempo_a; 
+              const dataY = value[0].traces_v; 
+
+              let dataText = '';
+
+              for (let i = 0; i < dataX.length; i++) {
+                dataText += dataX[i] + '     ' + dataY[i] + '\n'; 
+              }
+
+              const downloadLink = document.createElement('a');
+              downloadLink.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(dataText);
+              downloadLink.download = `DATA_VELOCIDAD_${e.network}.${e.station}.${e.location}.${e.channel}.txt`;
+
+              document.body.appendChild(downloadLink);
+              downloadLink.click();
+              document.body.removeChild(downloadLink);
+            }
+          }
         }
       },
       grid: {
@@ -912,7 +963,31 @@ export class LectorDemoComponent implements OnInit {
           },
           dataView: { readOnly: true },
           restore: {},
-          saveAsImage: {}
+          saveAsImage: {},
+          myDownloadButton: {
+            show: true,
+            title: 'Descargar Datos',
+            icon: "path://M18.344,16.174l-7.98-12.856c-0.172-0.288-0.586-0.288-0.758,0L1.627,16.217c0.339-0.543-0.603,0.668,0.384,0.682h15.991C18.893,16.891,18.167,15.961,18.344,16.174 M2.789,16.008l7.196-11.6l7.224,11.6H2.789z M10.455,7.552v3.561c0,0.244-0.199,0.445-0.443,0.445s-0.443-0.201-0.443-0.445V7.552c0-0.245,0.199-0.445,0.443-0.445S10.455,7.307,10.455,7.552M10.012,12.439c-0.733,0-1.33,0.6-1.33,1.336s0.597,1.336,1.33,1.336c0.734,0,1.33-0.6,1.33-1.336S10.746,12.439,10.012,12.439M10.012,14.221c-0.244,0-0.443-0.199-0.443-0.445c0-0.244,0.199-0.445,0.443-0.445s0.443,0.201,0.443,0.445C10.455,14.021,10.256,14.221,10.012,14.221",
+            onclick: function () {
+
+              const dataX = value[0].tiempo_a; 
+              const dataY = value[0].traces_d; 
+
+              let dataText = '';
+
+              for (let i = 0; i < dataX.length; i++) {
+                dataText += dataX[i] + '     ' + dataY[i] + '\n'; 
+              }
+
+              const downloadLink = document.createElement('a');
+              downloadLink.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(dataText);
+              downloadLink.download = `DATA_DESPLAZAMIENTO_${e.network}.${e.station}.${e.location}.${e.channel}.txt`;
+
+              document.body.appendChild(downloadLink);
+              downloadLink.click();
+              document.body.removeChild(downloadLink);
+            }
+          }
         }
       },
       grid: {
